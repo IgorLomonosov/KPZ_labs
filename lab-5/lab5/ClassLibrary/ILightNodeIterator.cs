fc183f30e101c3bb5fc736c2c0ba8c03fedb8206
Ігор Ломоносов <ipz224_lio@student.ztu.edu.ua>
2024-05-30 16:33:01 +0300

    First feature

---
+++ lab-5/lab5/ClassLibrary/ILightNodeIterator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public interface ILightNodeIterator
+    {
+        bool HasNext();
+        LightNode Next();
+    }
+
+    public interface IIterableLightNode
+    {
+        ILightNodeIterator CreateDepthFirstIterator();
+        ILightNodeIterator CreateBreadthFirstIterator();
+    }
+}
