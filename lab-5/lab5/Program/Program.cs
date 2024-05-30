b577252d1dbcacbcce8c84e25ba7a4b54efc2f01
Ігор Ломоносов <ipz224_lio@student.ztu.edu.ua>
2024-05-30 16:03:35 +0300

    Created everything needed for fifth lab

---
+++ lab-5/lab5/Program/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary;
+namespace task05
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var paragraph = new LightElementNode("p", "block", "normal", new List<string> { "paragraph" },
+                new List<LightNode> { new LightTextNode("Hello, World!") });
+
+            Console.WriteLine(paragraph.OuterHtml);
+        }
+    }
+
+}
