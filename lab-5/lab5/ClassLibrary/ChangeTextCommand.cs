6ded36c411e9c2e6a63bfd93e7cf2c18ab20c8f8
Ігор Ломоносов <ipz224_lio@student.ztu.edu.ua>
2024-05-30 16:43:28 +0300

    Added second feature Command Pattern

---
+++ lab-5/lab5/ClassLibrary/ChangeTextCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class ChangeTextCommand : ICommand
+    {
+        private readonly LightTextNode _textNode;
+        private readonly string _newText;
+        private string _oldText;
+
+        public ChangeTextCommand(LightTextNode textNode, string newText)
+        {
+            _textNode = textNode;
+            _newText = newText;
+        }
+
+        public void Execute()
+        {
+            _oldText = _textNode.Text;
+            _textNode.Text = _newText;
+        }
+
+        public void Undo()
+        {
+            _textNode.Text = _oldText;
+        }
+    }
+}
